2945ada19fd4b4c225b2117c8c211528ba32d4ba
Edmar Lelis Loureço Júnior <120860142+EdmarLelis@users.noreply.github.com>
2023-03-12 23:11:14 -0300

    atualizar cliente

--- templates/static/clientes/js/clientes.js
+++ templates/static/clientes/js/clientes.js
@@ -10,17 +10,35 @@ function add_carro(){
 function exibir_form(tipo){
     add_cliente = document.getElementById("adicionar-cliente")
     att_cliente = document.getElementById("att_cliente")
+    add = document.getElementById("add")
+    att = document.getElementById("att")
     
     if(tipo == "1"){
 
         att_cliente.style.display = "none"
         add_cliente.style.display = "block"
+        add.setAttribute('style', 'border: 2px solid white;')
+        att.setAttribute('style', 'border: none;')
+
+
 
     }else if(tipo == "2"){
 
         add_cliente.style.display = "none"
         att_cliente.style.display = "block"
+        att.setAttribute('style', 'border: 2px solid white;')
+        add.setAttribute('style', 'border: none;')
 
     }
+
+}
+
+function dados_cliente(){
+    cliente = document.getElementById("cliente-select")
+
+    fetch("/clientes/atualiza_cliente/", {
+        method: "POST",
+
+    })
 
 }
