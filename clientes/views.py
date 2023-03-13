d72ca37aea2e2d7e284be84e738046cd0fb4d4fe
Edmar Lelis Loureço Júnior <120860142+EdmarLelis@users.noreply.github.com>
2023-03-12 22:41:47 -0300

    select

--- clientes/views.py
+++ clientes/views.py
@@ -8,7 +8,8 @@ import re
 
 def clientes(request):
     if request.method == "GET":
-        return render(request, 'clientes.html')
+        clientes_list = Cliente.objects.all()
+        return render(request, 'clientes.html', {'clientes': clientes_list})
     elif request.method == "POST":
         nome = request.POST.get('nome')
         sobrenome = request.POST.get('sobrenome')
@@ -18,11 +19,7 @@ def clientes(request):
         carros = request.POST.getlist('carro')
         placas = request.POST.getlist('placa')
         anos = request.POST.getlist('ano')
-        
-        
-        if not re.fullmatch(re.compile(r'/^([0-9]{3}\.?[0-9]{3}\.?[0-9]{3}\-?[0-9]{2}|[0-9]{2}\.?[0-9]{3}\.?[0-9]{3}\/?[0-9]{4}\-?[0-9]{2})$/'), cpf):
-            return render(request, 'clientes.html', {'nome' : nome, 'sobrenome': sobrenome, 'email': email, 'carros': zip(carros, placas, anos)})
-        
+                
         cliente = Cliente.objects.filter(cpf= cpf)
         
         if cliente.exists():
