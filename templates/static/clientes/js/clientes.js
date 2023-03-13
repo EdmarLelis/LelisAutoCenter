6cd8e33f7f483e97554d7210f3045a4fd595e9b0
Edmar Lelis Loureço Júnior <120860142+EdmarLelis@users.noreply.github.com>
2023-03-12 22:15:15 -0300

    Opção Atualizar cliente

--- templates/static/clientes/js/clientes.js
+++ templates/static/clientes/js/clientes.js
@@ -6,4 +6,21 @@ function add_carro(){
 
     container.innerHTML += html
 }
+
+function exibir_form(tipo){
+    add_cliente = document.getElementById("adicionar-cliente")
+    att_cliente = document.getElementById("att_cliente")
     
+    if(tipo == "1"){
+
+        att_cliente.style.display = "none"
+        add_cliente.style.display = "block"
+
+    }else if(tipo == "2"){
+
+        add_cliente.style.display = "none"
+        att_cliente.style.display = "block"
+
+    }
+
+}
